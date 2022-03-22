1f05136c1765d6e7e388d8e27ebcc9bd56eb0d0a
jfilya <julia.94@bk.ru>
2022-03-22 17:39:23 +0300

    task Quarter of the year

--- codewars.js
+++ codewars.js
@@ -152,3 +152,16 @@ function lovefunc(flower1, flower2){
   }
   else return true;
 }
+
+// к какому кварталу года принадлежит месяц
+const quarterOf = (month) => {
+  if (month <=3){
+   return 1;
+ } else  if (month >=4 && month <=6){
+   return 2;
+  } else  if (month >=7 && month <=9){
+   return 3;
+  } else  if (month >=10 && month <=12){
+   return 4;
+  }   
+}
