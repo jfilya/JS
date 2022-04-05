ed1de8e2d258cfcde1960f25eeab33ac009c2387
Yuliua Filippova <95137071+jfilya@users.noreply.github.com>
2022-04-05 22:58:37 +0300

    feat: task Reversed sequence

--- codewars.js
+++ codewars.js
@@ -705,3 +705,14 @@ function sakuraFall(v) {
 function otherAngle(a, b) {
   return 180-(a+b);
 }
+
+
+
+//Создайте функцию, которая возвращает массив целых чисел от n до 1, где n>0. Пример: n=5 --> [5,4,3,2,1]
+const reverseSeq = n => {
+  let arr=[];
+  for (let i=n; i>0; i--){
+    arr.push(i);
+  }
+  return arr;
+};
